f94d6f94378d6de0bc87ef8cf9bd80082ad7de86
Ayoub Laghchim <ayoublaghchim77@gmail.com>
2025-04-26 14:18:53 +0100

    Modifications sur le fichier server.js et port

--- server.js
+++ server.js
@@ -73,5 +73,8 @@ app.use('/students', studentsRoutes);
 app.use('/loans', loansRoutes);
 
 // Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log('Server is successfully running...');
+});
